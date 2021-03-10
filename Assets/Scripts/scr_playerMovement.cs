aaecf0a11f4972ef6790b9a205785e1f2261e9ef
ShaneStrander <47073125+ShaneStrander@users.noreply.github.com>
2021-03-10 16:12:18 -0600

    Set up isometric view, player controller, and scene transitions

---
+++ Assets/Scripts/scr_playerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_playerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float moveH, moveV;
+    [SerializeField] private float moveSpeed = 5.0f;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveV = Input.GetAxisRaw("Vertical") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveH, moveV);
+    }
+}
+
+
+
+
+
+
