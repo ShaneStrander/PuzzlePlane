bc2da593fe7153f154881d34cf9138ebe8903e14
egarc21 <55520840+egarc21@users.noreply.github.com>
2021-04-05 16:24:32 -0500

    asteroid game added
    transition to asteroid works, small things missing from original but theyre isolated to asteroid game

---
+++ Assets/Scripts/scr_asteroidScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_asteroidScript : MonoBehaviour
+{
+    public float maxThrust;
+    public float maxTorque;
+    public Rigidbody2D rb;
+    public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+    public float asteroidSize; //2 == Large 1 == Medium
+    public GameObject asteroidMedium;
+    public int points;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //add random amount of torque and thrust to the asteroid
+        Vector2 thrust = new Vector2(Random.Range(-maxThrust, maxThrust), Random.Range(-maxThrust, maxThrust));
+        float torque = Random.Range(-maxTorque, maxTorque);
+        rb.AddForce(thrust);
+        rb.AddTorque(torque);
+
+       // player = GameObject.FindWithTag("Player").GetComponent<scr_spaceshipControls>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 newPos = transform.position;
+        //screen wrapping
+        if (transform.position.y > screenTop)
+        {
+            newPos.y = screenBottom;
+        }
+
+        if (transform.position.y < screenBottom)
+        {
+            newPos.y = screenTop;
+        }
+
+        if (transform.position.x > screenRight)
+        {
+            newPos.x = screenLeft;
+        }
+
+        if (transform.position.x < screenLeft)
+        {
+            newPos.x = screenRight;
+        }
+
+        transform.position = newPos;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("bullet"))
+        {
+            Destroy(other.gameObject);
+
+            if(asteroidSize == 2)
+            {
+                // spawn two medium asteroids
+                GameObject asteroid1 = Instantiate(asteroidMedium, transform.position, transform.rotation);
+                GameObject asteroid2 = Instantiate(asteroidMedium, transform.position, transform.rotation);
+                asteroid1.GetComponent<scr_asteroidScript>().asteroidSize = 1;
+                asteroid2.GetComponent<scr_asteroidScript>().asteroidSize = 1;
+
+                
+            }else if (asteroidSize == 1)
+            {
+
+            }
+            //player.ScorePoints(points);
+
+            Destroy(gameObject);
+
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/scr_spaceshipControls.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_spaceshipControls : MonoBehaviour
+{
+    public Rigidbody2D rb; 
+    public float thrust;
+    public float turnThrust;
+    private float thrustInput;
+    private float turnInput;
+    public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+    public float bulletForce;
+    public float deathForce;
+
+    public GameObject bullet;
+
+    private int score;
+    public int lives;
+
+    public Text scoreText;
+
+    public Text livesText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score;
+        livesText.text = "Lives: " + lives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check input from the keyboard 
+        thrustInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+
+        //check for input from the fire key and make bullets
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * bulletForce);
+            Destroy(newBullet, 5.0f);
+        }
+
+        //rotate  the ship
+        transform.Rotate(Vector3.forward * turnInput * Time.deltaTime * turnThrust);
+
+        Vector2 newPos = transform.position;
+        //screen wrapping
+        if(transform.position.y> screenTop)
+        {
+            newPos.y = screenBottom;
+        }
+
+        if (transform.position.y < screenBottom)
+        {
+            newPos.y = screenTop;
+        }
+        
+        if (transform.position.x > screenRight)
+        {
+            newPos.x = screenLeft;
+        }
+
+        if (transform.position.x < screenLeft)
+        {
+            newPos.x = screenRight;
+        }
+
+        transform.position = newPos;
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddRelativeForce(Vector2.up * thrustInput * thrust);
+        //rb.AddTorque(-turnInput * thrust);
+        
+    }
+
+    void ScorePoints(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.relativeVelocity.magnitude > deathForce)
+        {
+            lives--;
+            livesText.text = "Lives: " +  lives;
+            if (lives <= 0)
+            {
+                //Gameover
+            }
+        }
+    }
+
+}
