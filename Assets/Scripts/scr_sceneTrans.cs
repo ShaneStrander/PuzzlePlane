097fa373f6531b0756b841084ba5fdcde6252494
egarc21 <55520840+egarc21@users.noreply.github.com>
2021-04-14 10:38:54 -0500

    Transitions
    Added fade transitions and background for platform

--- Assets/Scripts/scr_sceneTrans.cs
+++ Assets/Scripts/scr_sceneTrans.cs
@@ -7,13 +7,24 @@ using UnityEngine.SceneManagement;
 public class scr_sceneTrans : MonoBehaviour
 {
     public string sceneToLoad;
+    public Animator transition;
+    public float transitionTime = 1f;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(LoadScene(sceneToLoad));
         }
+    }
+
+    IEnumerator LoadScene(string index)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(index);
     }
 
 }
