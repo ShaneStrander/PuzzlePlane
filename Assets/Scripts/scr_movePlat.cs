0f6fa134d7969510d509bf6fe9acfc04c38fba2e
ShaneStrander <47073125+ShaneStrander@users.noreply.github.com>
2021-04-14 20:41:34 -0500

    Overhaul platform minigame and add second level

---
+++ Assets/Scripts/scr_movePlat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_movePlat: MonoBehaviour
+{
+    public float rightLimit = 3.8f;
+    public float leftLimit = 0f;
+    public float speed = 0.0f;
+
+    private int direction = 1;
+    private Vector3 movement;
+
+    void Update()
+    {
+        
+        if (transform.position.x > rightLimit)
+        {
+            direction = -1;
+        }
+        else if (transform.position.x < leftLimit)
+        {
+            direction = 1;
+        }
+
+        movement = Vector3.right * direction * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+}
--- Assets/Scripts/scr_platformerMovement.cs
+++ Assets/Scripts/scr_platformerMovement.cs
@@ -5,60 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class scr_platformerMovement : MonoBehaviour
 {
-    //Movement
     public float speed;
-    public float jump;
-    float moveVelocity;
+    public float jumpForce;
+    private float moveInput;
 
-    //Grounded Vars
-    bool grounded = true;
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
 
     void Update()
     {
-        //Jumping
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (grounded)
+            if (isGrounded)
             {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+                rb.velocity = Vector2.up * jumpForce;
             }
         }
-
-        moveVelocity = 0;
-
-        //Left Right Movement
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            moveVelocity = -speed;
-        }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            moveVelocity = speed;
-        }
-
-        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
-
     }
-    //Check if Grounded
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Platform")
-        {
-            grounded = true;
-        }
-
         if (collision.CompareTag("FinishLine"))
         {
-            SceneManager.LoadScene("Scene4");
+            SceneManager.LoadScene("ScenePirPlat2");
         }
-
-    }
-
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Platform")
+        if (collision.CompareTag("FinishLine2"))
         {
-            grounded = false;
+            SceneManager.LoadScene("Scene4");
         }
     }
 
